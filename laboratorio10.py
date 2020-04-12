d23c0df862c57358040a31644db0f9384df0def5
Armando Enrique Cortés <62660438+Armandosky@users.noreply.github.com>
2020-04-12 17:04:50 -0500

    Modificación con contador y paridad

--- laboratorio10.py
+++ laboratorio10.py
@@ -6,17 +6,27 @@ Created on Fri Apr  3 11:25:01 2020
 """
 
 
-def a_power_b(a,b):
+def a_power_b(a, b):
     potencia = 1
     while (b>0):
         potencia = potencia*a
         b = b - 1
-    print("el resultado es: "+str(potencia))
     return potencia
 
-
 a=1
-while a<0 or a>0:
-    a = int(input('Ingrese base:  '))
-    b = abs(int(input('Ingrese exponente:  ')))
-    a_power_b(a,b)
+c1=0
+c2=0
+c3=0
+while a!=0:
+    a = int(input('Ingrese base: '))
+    b = abs(int(input('Ingrese exponente: ')))
+    c3 = c3 + 1
+    if a_power_b(a, b)%2 != 0:
+       c1 += 1
+    elif a_power_b(a, b)%2 == 0:
+        c2 += 1
+    print("El resultado es: "+str(a_power_b(a, b)))
+print("El número de impares es: "+str(c1))
+print("El número de pares es: "+str(c2))
+print("El número de potencias es: "+str(c3 - 1))
+
